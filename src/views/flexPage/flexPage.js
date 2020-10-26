f860b2d943404dfb4595cdac6711b9601d5892c8
qiushangzhe <470542573@qq.com>
2020-10-26 23:16:45 +0800

    初始化项目

---
+++ src/views/flexPage/flexPage.js
@@ -0,0 +1,18 @@
+export default {
+  components: {},
+  props: [],
+  name: 'flex-page',
+  data () {
+    return {}
+  },
+  computed: {
+    // x: function() {}
+  },
+  watch: {
+    // x: function(val) {}
+  },
+  created () {},
+  mounted () {},
+  destroyed () {},
+  methods: {}
+}
---
+++ src/views/flexPage/index.vue
@@ -0,0 +1,4 @@
+
+<template src="./flexPage.html"></template>
+<script src="./flexPage.js" lang="js"></script>
+<style src="./flexPage.less" lang="less"></style>
